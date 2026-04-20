2a8113b701e68297d43334dcf1f54ef0ed7490d9
lpdodgers <ferries.calm4r@icloud.com>
2026-04-20 12:14:28 -0500

    Attempting to find Service time and Arrival time with NumPy exponential

--- M-M-1 Queue.py
+++ M-M-1 Queue.py
@@ -1,40 +1,42 @@
 from cmath import inf
 import pandas as pd
+import numpy as np
 
 def Get_Arrival():
-    pass
+
+    return None
 
 def Get_Service():
-    pass
+    np.random.exponential(scale=1)
+    return None
 
 def main():
-    Next_Arrival = Get_Arrival()
-    Next_Depart = inf
-    Utilization = 0
-    Queue = 0
-    Queue_Time = 0
-    Sys_Time = 0
-    Time = 0
-
-def idk():
-    while Time <= 0:
-        if Next_Arrival < Next_Depart:
-            Time = Next_Arrival
+    next_arrival = Get_Arrival()
+    next_depart = inf
+    utilization = 0
+    queue = 0
+    queue_time = 0
+    sys_time = 0
+    time = 0
+    max_time = 500
+    while time <= 500:
+        if next_arrival < next_depart:
+            time = next_arrival
             if utilization == 0:
                 utilization = 1
-                Next_Depart = Get_Service()
+                next_depart = Get_Service()
             else:
                 queue += 1
-            Next_Arrival = Get_Arrival()
+            next_arrival = Get_Arrival()
             #Update Q_Time & Sys_Time
         else:
-            Time = Next_Arrival
+            time = next_arrival
             if queue >= 1:
                 queue -= 1
-                Next_Depart = Get_Service()
+                next_depart = Get_Service()
             else:
                 utilization = 0
-                Next_Depart = inf
+                next_depart = inf
                 #Update Q_Time & System_Time
 
 
