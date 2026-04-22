823a95332c5aedd09839005a798f6081e8c5743a
lpdodgers <ferries.calm4r@icloud.com>
2026-04-21 20:47:56 -0500

    Added SciPy for CI, removed random seed, implemented ci with t value and margin of error

--- M-M-1 Queue.py
+++ M-M-1 Queue.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.random.seed(0)
+import scipy.stats as sc
 
 def Get_Arrival():
     return np.random.exponential(scale=1/3)
@@ -61,6 +61,15 @@ def running():
     #print(f'Average Utilization: {avg_utilization}')
     return avg_queue_time, avg_sys_time, avg_queue_length, avg_sys_length, avg_utilization
 
+def CI(dat):
+    n = len(dat)
+    sample_mean = np.mean(dat)
+    sample_std = np.std(dat, ddof = 1)
+    t_value = sc.t.ppf(.975, df = n-1)
+    margin_of_error = t_value * (sample_std / np.sqrt(n))
+    ci = float(sample_mean - margin_of_error), float(sample_mean + margin_of_error)
+    return ci
+
 def main():
         n = int(input(f'Number of Replications: '))
         results = []
@@ -68,11 +77,16 @@ def main():
             results.append(running())
         results = np.array(results)
         outputs = results.mean(axis=0)
-        print(f'Average Queue Time: {outputs[0]}')
-        print(f'Average System Time: {outputs[1]}')
-        print(f'Average Queue Length: {outputs[2]}')
-        print(f'Average System Length: {outputs[3]}')
-        print(f'Average Utilization: {outputs[4]}')
+
+        #Confidence Intervals | alpha = .05 | see CI
+
+        #Print
+        print(f'Average Queue Time: {outputs[0]}, CI: {CI(results[:,0])}')
+        print(f'Average System Time: {outputs[1]}, CI: {CI(results[:, 1])}')
+        print(f'Average Queue Length: {outputs[2]}, CI: {CI(results[:, 2])}')
+        print(f'Average System Length: {outputs[3]}, CI: {CI(results[:, 3])}')
+        print(f'Average Utilization: {outputs[4]}, CI: {CI(results[:, 4])}')
+
 
 if __name__ == "__main__":
     main()
